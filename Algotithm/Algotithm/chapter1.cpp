77a39c86ce73958d376beff30a16203a29407bf5
unknown <1433385675@qq.com>
2019-03-21 11:03:52 +0800

    vs init

---
+++ Algotithm/Algotithm/chapter1.cpp
@@ -0,0 +1,5 @@
+#include "chapter_1_data_structure_basis.h"
+void fun()
+{
+	cout << "hello world" << endl;
+}
---
+++ Algotithm/Algotithm/main.cpp
@@ -0,0 +1,7 @@
+#include "chapter_1_data_structure_basis.h"
+
+int main()
+{
+	fun();
+	return 0;
+}
